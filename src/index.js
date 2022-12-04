8274fe45f176f0c6ef972c764cafa4377bb71ecf
豆腐 <remuxia1415erina@gmail.com>
2022-12-04 14:12:54 +0000

    Update 8 files

--- src/index.js
+++ src/index.js
@@ -48,3 +48,17 @@
 // // 上のは、myProfileと長い
 // const { name, age} = myProfile;
 // const message4 = `名前は${name}です。年齢は${age}です。`
+
+// const myProfile = ["とーふ", "102"];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`;
+// console.log(message4);
+
+/**デフォルト値 引数 */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちわ!${name}さん`);
+// sayHello("とーふ");
+
+// スプレット構文
