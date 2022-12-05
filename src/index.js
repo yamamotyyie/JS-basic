cab1513e1eb4e330b116d7fd0ca7595081055cf3
豆腐 <remuxia1415erina@gmail.com>
2022-12-05 14:57:18 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -62,3 +62,33 @@
 // sayHello("とーふ");
 
 // スプレット構文
+// ...を連続で書く
+// 配列の展開
+// const arr1 = [1,2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1,num2)=>console.log(num1+num2);
+// sumFunc(arr1[0],arr1[1]);
+// sumFunc(...arr1);
+// 配列に対して使ってる。順番を変換
+
+// 一つにまとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1,num2,...arr3]=arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4,...arr5];
+// console.log(arr7);
+
+const arr8 = arr4;
+console.log(arr8);
